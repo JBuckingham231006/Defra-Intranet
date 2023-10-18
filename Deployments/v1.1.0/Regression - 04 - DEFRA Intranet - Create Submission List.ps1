20170ea1ea9d642282088628d86095a7b0adf6a7
misonn <34516266+misonn@users.noreply.github.com>
2023-10-18 12:52:04 +0100

    Commiting scripts for version 1.1.0

---
+++ Deployments/v1.1.0/Regression - 04 - DEFRA Intranet - Create Submission List.ps1
@@ -0,0 +1,71 @@
+<#
+    SCRIPT OVERVIEW:
+    This script uninstalls our custom submission list
+
+    SOFTWARE REQUIREMENTS:
+    This script was developed on the following platform:
+        PowerShell v5.1.22621.1778
+        SharePointPnPPowerShellOnline v3.29.2101.0
+
+    SHAREPOINT PERMISSIONS REQUIREMENTS:
+    - Site Collection Admins rights to the DEFRA Intranet SharePoint site
+    OR
+    - Access to the SharePoint Tenant Administration site
+#>
+
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Stop"
+
+Import-Module SharePointPnPPowerShellOnline
+
+if ($null -ne $psISE)
+{
+    $logfileName = $($psISE.CurrentFile.FullPath.Split('\'))[$psISE.CurrentFile.FullPath.Split('\').Count-1]
+    $logfileName = $logfileName.Replace(".ps1",".txt")
+
+    $global:scriptPath = Split-Path -Path $psISE.CurrentFile.FullPath
+
+    Import-Module "$global:scriptPath\PSModules\Configuration.psm1" -Force
+    Import-Module "$global:scriptPath\PSModules\Helper.psm1" -Force
+}
+else
+{
+    Clear-Host
+
+    $logFileName = $MyInvocation.MyCommand.Name
+    $global:scriptPath = "."
+
+    Import-Module "./PSModules/Configuration.psm1" -Force
+    Import-Module "./PSModules/Helper.psm1" -Force
+}
+
+$logfileName = $logfileName.Replace(".ps1",".txt")
+Start-Transcript -path "$global:scriptPath/Logs/$logfileName" -append | Out-Null
+
+Invoke-Configuration
+
+$site = $global:sites | Where-Object { $_.Abbreviation -eq "DEFRA" -and $_.RelativeURL.Length -gt 0 }
+
+if($null -eq $site)
+{
+    throw "An entry in the configuration could not be found for the 'DEFRA Intranet' or is not configured correctly"
+}
+
+Connect-PnPOnline -Url "$global:rootURL/$($site.RelativeURL)" -UseWebLogin
+Write-Host "SCRIPT EXECUTED BY '$(Get-CurrentUser)' AT $(get-date -f "HH:mm:ss") ON $(get-date -f "dd/MM/yyyy")" -ForegroundColor Cyan
+Write-Host "ACCESSING SHAREPOINT SITE: $($global:rootURL)/$($global:site.RelativeURL)" -ForegroundColor Cyan
+
+$displayName = "Internal Comms Intranet Content Submissions"
+$listURL = "Lists/ICICS"
+
+$list = Get-PnPList -Identity $listURL
+
+if($null -ne $list)
+{
+    Remove-PnPList -Identity $listURL -Force
+    Write-Host "THE '$displayName' LIST HAS BEEN REMOVED" -ForegroundColor Green
+}
+
+Write-Host "SCRIPT FINISHED" -ForegroundColor Yellow
+Stop-Transcript
