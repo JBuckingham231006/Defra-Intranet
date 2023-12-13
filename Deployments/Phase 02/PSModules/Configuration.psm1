f62742ca0c149693b3ca06855883b80e74cdb242
James Buckingham <james@buckinghamdevelopment.co.uk>
2023-12-13 00:27:31 +0000

    New PowerShell scripts for copying the values of the Organisation Intranet column into the new Content Editor Organisation Intranet column

--- Deployments/Phase 02/PSModules/Configuration.psm1
+++ Deployments/Phase 02/PSModules/Configuration.psm1
@@ -12,7 +12,7 @@
 #>
 
 # GLOBAL SETTINGS
-$global:environment = 'dev' # Which environment are we targetting?
+$global:environment = 'local001' # Which environment are we targetting?
 
 # GLOBAL VARIABLES
 
@@ -22,7 +22,7 @@ if($global:PSScriptRoot.Length -gt 0)
     New-Item -ItemType Directory -Force -Path "$global:PSScriptRoot\Logs" | Out-Null
 }
 else
-{
+{ 
     New-Item -ItemType Directory -Force -Path "./Logs" | Out-Null
 }
 
